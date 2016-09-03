ab67668279f6a4cdd03cdd99cd225abeb4bc5cff
Hanna Andersson <hannaa78@gmail.com>
2016-09-03 21:26:35 +0200

    more angular

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -1,9 +1,14 @@
 'use strict';
 
 app.controller('myCtrl', function($scope, dataService, $firebaseObject) {
-	const rootRef = firebase.database().ref().child('angular');
-	const ref = rootRef.child('object');
-	this.object = $firebaseObject(ref);
+
+	var ref = firebase.database().ref();
+	var firebaseObject = $firebaseObject(ref);
+
+	firebaseObject.$bindTo($scope,"event");
+	//const rootRef = firebase.database().ref().child('angular');
+	//const ref = rootRef.child('object');
+	//this.object = $firebaseObject(ref);
 
 
 /* */
