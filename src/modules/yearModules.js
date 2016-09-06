8e48dcf1626d6c2f1bd99747a663493bc0eae75d
robertlindell <robert.lindellnordin@gmail.com>
2016-09-06 14:17:31 +0200

    added test files

---
+++ src/modules/yearModules.js
@@ -0,0 +1,11 @@
+exports.checkYear = function(inputYear) {
+
+	inputYear = inputYear.equal(num)
+	inputYear = inputYear.equal(4)
+	
+
+	var output = inputYear;
+
+	return output;
+}
+
---
+++ src/test/checkYear.js
@@ -0,0 +1,40 @@
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+
+var modules = require('../modules/modules.js');
+
+
+describe('Check that year is a number.', function() {
+
+	it('Year is a num', function(){
+		var year = modules.checkYear(year);
+		expect(output).to.be.a(num);		
+	});
+/*
+	it('has four digits', function(){
+		var year = modules.checkYear(year);
+		expect(output).to.have.length.of.at.least(4);
+	});
+
+	it('has four digits', function(){
+		var year = modules.checkYear(year);
+		expect(output).to.have.length.below(5);
+	});
+*/
+	it('has four digits', function(){
+		var year = modules.checkYear(year);
+		expect(output).to.have.lengthof(4);
+	});
+
+	it('year is later than 1800', function(){
+		expect(output).to.be.at.least(1800);
+	});
+});
+
+
+describe('question is a string', function(){
+
+	it('question is a string', function(){
+		expect(question).to.be.a(string);
+	});
+});
