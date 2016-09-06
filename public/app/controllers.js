90c8257ec6eca39f3d7975124df285ef619e8265
Hanna Andersson <hannaa78@gmail.com>
2016-09-06 15:16:08 +0200

    Update Firebase

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -4,11 +4,14 @@ module.exports = angular.module('app').controller('myCtrl', ["$scope", "$firebas
 // angular.module('app').controller('myCtrl', ["$scope", "$firebaseObject", "$firebaseArray", function($scope, $firebaseObject, $firebaseArray) {
 
 // app.controller('myCtrl', ["$scope", "$firebaseObject", "$firebaseArray", function($scope, $firebaseObject, $firebaseArray) {
+ 
 
   var ref = firebase.database().ref();
   var firebaseObject = $firebaseObject(ref);
 
   $scope.events = $firebaseArray(ref);
+    $scope.name = "abc";
+    console.log($scope.name);
 /*
  function myFunction(a, b) {
     return a * b;
@@ -20,8 +23,16 @@ module.exports = angular.module('app').controller('myCtrl', ["$scope", "$firebas
 */
 
   $scope.add = function() {
-    $scope.events.$add($scope.what);
-  }
+
+    $scope.events.$add({
+      'name': $scope.event.name,
+      'year': $scope.event.year,
+    
+    }).then(function(data){
+
+    });
+   
+  };
 
   //firebaseObject.$bindTo($scope,"event");
   //const rootRef = firebase.database().ref().child('angular');
