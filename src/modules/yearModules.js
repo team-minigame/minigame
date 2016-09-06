c907647e6cb643dd76ea7c9f7b4a670041e8f0d4
robertlindell <robert.lindellnordin@gmail.com>
2016-09-06 15:28:14 +0200

    update test

--- src/modules/yearModules.js
+++ src/modules/yearModules.js
@@ -3,8 +3,10 @@ exports.checkYear = function(inputYear) {
 	inputYear = inputYear.equal(num)
 	inputYear = inputYear.equal(4)
 	
+	assert.isAbove(inputYear1, inputYear2, 'inputYear1 is after inputYear2');
+	assert.isBelow(inputYear1, inputYear2, 'inputYear1 is before inputYear2');
 
-	var output = inputYear;
+	var output = ;
 
 	return output;
 }
